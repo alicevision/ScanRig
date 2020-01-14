5bbdd7953b53275a8bc4afcd2e4a9ec6b24b921a
Julien-Haudegond <44610840+Julien-Haudegond@users.noreply.github.com>
2020-01-14 17:16:59 +0100

    Update the camera script (works but not perfect)
    - Settings are working since we open cameras twice

--- bin/multiCameraCapture.py
+++ bin/multiCameraCapture.py
@@ -1,3 +1,4 @@
+#------------------------- IMPORTS
 import numpy as np
 import cv2
 
@@ -5,7 +6,7 @@ import os
 import argparse
 import logging
 
-
+#------------------------- LOGGING
 log_levels = {
     0: logging.CRITICAL,
     1: logging.ERROR,
@@ -14,6 +15,8 @@ log_levels = {
     4: logging.DEBUG,
 }
 
+
+#------------------------- ARGUMENTS PARSING
 parser = argparse.ArgumentParser(description='Display video stream')
 parser.add_argument('-o', '--output', metavar='Output folder', type=str,
                     default='',
@@ -38,37 +41,31 @@ parser.add_argument("-v", "--verbose", dest="verbosity", action="count", default
 args = parser.parse_args()
 logging.basicConfig(level=log_levels[args.verbosity])
 
-print("Press 'esc' to exit.")
-
 if args.output and not os.path.exists(args.output):
     os.mkdir(args.output)
 
 logging.info("Args: " + str(args))
+logging.info("Press 'esc' to exit.")
 
-captureDevices = []
 
-cameraApiID = 0
-for cameraIndex in args.cameras:
-    cap = cv2.VideoCapture()
-    v = cap.open(cameraApiID + cameraIndex)
-    if not v:
-        logging.warning("Skip invalid stream ID {}".format(cameraIndex))
-        cap.release()
-        continue
-
+#------------------------- SETTINGS FUNCTIONS
+def setAttributes(cap):
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 4208)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 3120)
-    cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0) # Disable auto-exposure
-    cap.set(cv2.CAP_PROP_EXPOSURE, 817) # From 0 to 10 000
-    cap.set(cv2.CAP_PROP_GAIN, 0) # From 0 to 100
-    cap.set(cv2.CAP_PROP_AUTO_WB, 0) # Disable auto-white balance
-    cap.set(cv2.CAP_PROP_WB_TEMPERATURE, 5600) # From 0 to 10 000
-    cap.set(cv2.CAP_PROP_SHARPNESS, 0)
     cap.set(cv2.CAP_PROP_BRIGHTNESS, 0)
     cap.set(cv2.CAP_PROP_CONTRAST, 0)
     cap.set(cv2.CAP_PROP_SATURATION, 32)
+    cap.set(cv2.CAP_PROP_WB_TEMPERATURE, 4500) # From 0 to 10 000
+    cap.set(cv2.CAP_PROP_AUTO_WB, 0) # Disable auto-white balance
     cap.set(cv2.CAP_PROP_GAMMA, 220)
+    cap.set(cv2.CAP_PROP_GAIN, 0) # From 0 to 100
+    cap.set(cv2.CAP_PROP_SHARPNESS, 0)
+    cap.set(cv2.CAP_PROP_EXPOSURE, 103) # From 0 to 10 000
+    cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1) # Disable auto-exposure
 
+    return
+
+def getAttributes(cap):
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     exposure = cap.get(cv2.CAP_PROP_EXPOSURE)
@@ -80,15 +77,57 @@ for cameraIndex in args.cameras:
     sharpness = cap.get(cv2.CAP_PROP_SHARPNESS)
     fps = cap.get(cv2.CAP_PROP_FPS)
     wb = cap.get(cv2.CAP_PROP_WB_TEMPERATURE)
-    uid = cap.get(cv2.CAP_PROP_GUID)
-    logging.info(f'uid={uid}, width={width}, height={height}, fps={fps}, exposure={exposure}, gain={gain}, wb={wb}, bright{brightness}, contrast{contrast}, sat{saturation}, sharp{sharpness}')
+    logging.info(f'width={width}, height={height}, fps={fps}, exposure={exposure}, gain={gain}, wb={wb}, bright{brightness}, contrast{contrast}, sat{saturation}, sharp{sharpness}')
+    logging.info(f'Backend = {cap.get(cv2.CAP_PROP_BACKEND)}')
+
+    return
+
+
+def initCamSettings():
+    cameraApiID = 0
+    for cameraIndex in args.cameras:
+        cap = cv2.VideoCapture()
+        v = cap.open(cameraApiID + cameraIndex, apiPreference=cv2.CAP_V4L2)
+        if not v:
+            logging.warning("Skip invalid stream ID {}".format(cameraIndex))
+            cap.release()
+            continue
+
+        setAttributes(cap)
+
+        ret, frame = cap.read() # Read a frame seems to be required to make it work
+        cap.release()
+
+    return
+
+
+
+#------------------------- SCRIPT
+
+captureDevices = []
+cameraApiID = 0
+
+frameNumber = 0
+capFrame = 0
+
+initCamSettings() # Initialize camera settings (open/close cameras once seems to be required)
+
+# Initialize every camera
+for cameraIndex in args.cameras:
+    cap = cv2.VideoCapture()
+
+    v = cap.open(cameraApiID + cameraIndex, apiPreference=cv2.CAP_V4L2)
+    if not v:
+        logging.warning("Skip invalid stream ID {}".format(cameraIndex))
+        cap.release()
+        continue
+
+    setAttributes(cap)
+    getAttributes(cap)
 
     captureDevices.append((cameraIndex, cap))
 
-frameNumber = 0
-
-capFrame = 0
-
+# Main loop
 running = True
 while(running):
     frames = []
@@ -101,38 +140,40 @@ while(running):
             continue
         frames.append(frame)
         frameIdx.append(idx)
-        # height, width = frame.shape[:2]
-        # print('width={}, height={}'.format(width, height))
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
+    
     if args.display:
         VERTICAL = 0
         HORIZONTAL = 1
-        vis = np.concatenate(frames, axis=HORIZONTAL)
-        # Display the resulting frame
-        vis = cv2.resize(frame, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_AREA)
+        vis = np.concatenate(frames, axis=VERTICAL)
+        vis = cv2.resize(vis, None, fx=0.1, fy=0.1, interpolation=cv2.INTER_AREA)
         cv2.imshow('ScanRig', vis)
-        if k == 27: #ECHAP
-            running = False
-            break
+        
+    if k == 27: #ECHAP
+        running = False
+        break
 
-        if k == 32: #ESPACE
-            logging.info("Image captured")
-            filename = f'img_{capFrame}.jpg'
-            capFrame += 1
-            cv2.imwrite(filename=filename, img=frame)
+    if k == 32: #ESPACE
+        for idx, img in zip(frameIdx, frames):
+            outFilepath = f'cam{idx}_{capFrame:06d}.{args.extension}'
+            logging.info(f'Writting file={outFilepath}')
+            cv2.imwrite(outFilepath, img)
+        capFrame += 1
 
     if args.output:
         for idx, img in zip(frameIdx, frames):
-            outFilepath = '{outFolder}/cam{camId}_{frame:06d}.{ext}'.format(outFolder=args.output, camId=idx, frame=frameNumber, ext=args.extension)
-            logging.info('Writting file={}'.format(outFilepath))
+            outFilepath = f'{args.output}/cam{idx}_{capFrame:06d}.{args.extension}'
+            logging.info(f'Writting file={outFilepath}')
             cv2.imwrite(outFilepath, img)
+        capFrame += 1
+    else:
+        logging.info(f"frame {frameNumber}")
 
     frameNumber += 1
 
 # When everything done, release the capture devices
 for idx, cap in captureDevices:
     cap.release()
+
 
 # When everything done, release the window
 if args.display:
