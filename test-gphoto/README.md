34ca696bf4d76214e4772780ebb7fad8f7b40e78
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2020-01-24 17:05:19 +0100

    Change settings of dslr

--- test-gphoto/README.md
+++ test-gphoto/README.md
@@ -4,3 +4,5 @@ pip3 install -v gphoto2
 ```
 
 You need to unmount your device or gphoto cannot claim the usb device
+
+Check examples at https://github.com/jim-easterbrook/python-gphoto2/tree/master/examples
--- test-gphoto/camera.py
+++ test-gphoto/camera.py
@@ -4,7 +4,23 @@ context = gp.Context()
 camera = gp.Camera()
 camera.init(context)
 text = camera.get_summary(context)
+
+# TODO adapt this code https://github.com/jim-easterbrook/python-gphoto2/blob/master/examples/camera-config-gui.py
+
+config = camera.get_config()
+
+for child in config.get_children():
+    print('=======')
+    print('--- ' + child.get_label() + ' ---')
+    for prop in child.get_children():
+        print(prop.get_label())
+        if (prop.get_label() == 'Color Temperature'):
+            prop.set_value('2500')
+    
 print('Summary')
 print('=======')
 print(str(text))
+
+camera.set_config(config)
+
 camera.exit(context)
