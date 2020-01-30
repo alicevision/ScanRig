ea2cefccd338bb939668417f4b3a41ec05114fdb
dsmtE <desmet.enguerrand@gmail.com>
2020-01-30 20:42:06 +0100

    fix arduino initialisation

--- Test/testSerialParsing/testSerialParsing.ino
+++ Test/testSerialParsing/testSerialParsing.ino
@@ -6,8 +6,8 @@
 # define _DIR_5v 3 // DIR+(+5v) motor direction control  Brown
 # define _EN_5v  4 // ENA+(+5V) stepper motor enable     Yellow
 
-const char * cmdDelimiter = ":";
-const char * argumentDelimiter = ",";
+const char* cmdDelimiter = ":";
+const char* argumentDelimiter = ",";
 
 const int serialbufferSize = 255;
 
@@ -15,8 +15,18 @@ char serialBuffer[serialbufferSize];
 int serialIndex = 0; // where we are in the buffer  
 
 void setup() {
-  
-  Serial.begin(115200); //enable Serial Monitor connection in 115200 baud to control from python
+  //Sets the pins as Outputs
+	for (int i = 8; i <= 13; i++) { // relai Outputs
+		pinMode (i, OUTPUT);
+		digitalWrite(i, LOW);
+	}
+
+	pinMode (_EN_5v, OUTPUT); // ENA+(+5V)
+	pinMode (_DIR_5v, OUTPUT); // DIR+(+5v)
+	pinMode (_STP_5v, OUTPUT); // PUL+(+5v)
+	
+  Serial.begin(115200); //enable Serial Monitor connection in 115200 baud to control from python7
+  //Serial.println("start");
   // delay(10);
   // Serial.flush();
 }
@@ -92,7 +102,7 @@ int smoothMove(bool dir, bool inOut, long angle, long durationForOneTurn) {
 
   long x = 0;
 	for(; x < pulseNb; x++) {
-		float i = easeSigmoid(inOutDirection * (float(x)/float(pulseNb) - 0.5));
+		float i = easeSigmoid((float)inOutDirection * (float(x)/float(pulseNb) - 0.5));
 		float iterpolatedDuration = (float)demiStepDuration * (float)i;
 		digitalWrite(_STP_5v, HIGH);
 		delayMicroseconds(iterpolatedDuration);
@@ -152,14 +162,14 @@ String handleCmd(char* cmdBuffer) {
     return "Error: parsing cmd error";
   }
 
-  char *arg0 = strtok(cmdBuffer, argumentDelimiter);
-  char *arg1 = strtok(NULL, argumentDelimiter);
-  char *arg2 = strtok(NULL, argumentDelimiter);
-  char *arg3 = strtok(NULL, argumentDelimiter);
-  char *arg4 = strtok(NULL, argumentDelimiter);
+  char* arg0 = strtok(NULL, argumentDelimiter);
+  char* arg1 = strtok(NULL, argumentDelimiter);
+  char* arg2 = strtok(NULL, argumentDelimiter);
+  char* arg3 = strtok(NULL, argumentDelimiter);
+  char* arg4 = strtok(NULL, argumentDelimiter);
   
   if (strncmp(cmdBuffer, "led", 3) == 0) {
-		if (!arg0 || !arg1) { 
+		if (arg0 == NULL || arg1 == NULL) { 
 			return "Error: invalid number of arguments (led number[1, 6] & state(0 or 1) needed)";
 		}
     int ledNb = atoi(arg0);
@@ -171,23 +181,32 @@ String handleCmd(char* cmdBuffer) {
     setLedState(ledNb, state);
     return "Success: led " + String(ledNb) + (state ? "on": "off");
 
-  } else if (strncmp(cmdBuffer, "stop", 4) == 0 || strncmp(cmdBuffer, "s", 1) == 0) {
+  } else if (strncmp(cmdBuffer, "stop", 4) == 0) {
 		return "Success: motor stop";
 
   }else {
-    if(!arg0) {
+    if(arg0 == NULL) {
       return "Error: invalid first argument direction must be 1 for left or 0 for right";
     }
     int dir = atoi(arg0);
 
     if (strncmp(cmdBuffer, "captureFull", 11) == 0) {
-      if (!arg1 || !arg2 || !arg3 || !arg4) { 
+      if (arg1 == NULL || arg2 == NULL || arg3 == NULL || arg4 == NULL) {
 			  return "Error: invalid number of arguments (angle(deg), captureAngle(deg), smoothAngle(deg) & durationForOneTurn(sec/tr) needed)";
       }
       int angle = atoi(arg1);
       int captureAngle = atoi(arg2);
       int smoothAngle = atoi(arg3);
       int durationForOneTurn = atoi(arg4);
+
+      // Serial.print("Error :");
+      // Serial.print(angle);
+      // Serial.print("-");
+      // Serial.print(captureAngle);
+      // Serial.print("-");
+      // Serial.print(smoothAngle);
+      // Serial.print("-");
+      // Serial.println(durationForOneTurn);
 
       int angleDone = captureFull(dir, angle, captureAngle, smoothAngle, durationForOneTurn);
       if( angleDone == angle) {
@@ -197,7 +216,7 @@ String handleCmd(char* cmdBuffer) {
       } 
 
     }else if (strncmp(cmdBuffer, "capture", 7) == 0) {
-      if (!arg1 || !arg2 || !arg3 || !arg4) { 
+      if (arg1 == NULL || arg2 == NULL || arg3 == NULL || arg4 == NULL) { 
 			  return "Error: invalid number of arguments (angle(deg), captureAngle(deg), smoothAngle(deg) & durationForOneTurn(sec/tr) needed)";
       }
       int angle = atoi(arg1);
@@ -213,7 +232,7 @@ String handleCmd(char* cmdBuffer) {
       }
 
     }else if (strncmp(cmdBuffer, "smooth", 6) == 0) {
-      if (!arg1 || !arg2 || !arg3) {
+      if (arg1 == NULL || arg2 == NULL || arg3 == NULL) {
 			  	return "Error: invalid number of arguments easing direction(0 or 1), angle(deg) & durationForOneTurn(sec/tr) needed)";
       }
       int easeDir = atoi(arg1);
@@ -228,7 +247,7 @@ String handleCmd(char* cmdBuffer) {
       }
 
     }else if (strncmp(cmdBuffer, "move", 4) == 0) {
-      if (!arg1 || !arg2) {
+      if (arg1 == NULL || arg2 == NULL) {
 			  	return "Error: invalid number of arguments , angle(deg) & durationForOneTurn(sec/tr) needed)";
       }
       int angle = atoi(arg1);
@@ -242,7 +261,7 @@ String handleCmd(char* cmdBuffer) {
       }
 
     }else if (strncmp(cmdBuffer, "manual", 6) == 0) {
-      if (!arg1 || !arg2) {
+      if (arg1 == NULL || arg2 == NULL) {
 			  	return "Error: invalid number of arguments (pulseNumber & pulseDelay needed)";
       }
       int pulseNumber = atoi(arg1);
