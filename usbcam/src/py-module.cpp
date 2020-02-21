3138cb4459d6e13043a5a934e9a5ecd172221700
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2020-02-21 14:59:06 +0100

    Fix python error

--- usbcam/src/py-module.cpp
+++ usbcam/src/py-module.cpp
@@ -6,15 +6,16 @@ namespace py = pybind11;
 
 namespace USBCam {
 
-    PYBIND11_MODULE(GetDevicesList, m) {
+    PYBIND11_MODULE(usbcam, m) {
         m.def("GetDevicesList", &GetDevicesList, "Get connected devices");
     }
 
+/*
     PYBIND11_MODULE(CaptureManager, m) {
     py::class_<CaptureManager>(m, "CaptureManager")
         .def(py::init<std::vector<uint32_t>>())
         .def("GetCam", &CaptureManager::GetCam)
         .def("TakeAndSavePictures", &CaptureManager::TakeAndSavePictures);
     }
-
+*/
 }
