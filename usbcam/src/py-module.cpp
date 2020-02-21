6a2a797cf14acb0dca19f854c8d6b178741e99bb
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2020-02-21 15:20:22 +0100

    Get device list

--- usbcam/src/py-module.cpp
+++ usbcam/src/py-module.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include "capture-manager.h"
 
@@ -8,14 +9,14 @@ namespace USBCam {
 
     PYBIND11_MODULE(usbcam, m) {
         m.def("GetDevicesList", &GetDevicesList, "Get connected devices");
-    }
 
-/*
-    PYBIND11_MODULE(CaptureManager, m) {
-    py::class_<CaptureManager>(m, "CaptureManager")
-        .def(py::init<std::vector<uint32_t>>())
-        .def("GetCam", &CaptureManager::GetCam)
-        .def("TakeAndSavePictures", &CaptureManager::TakeAndSavePictures);
+        py::class_<Port>(m, "Port")
+            .def_readonly("number", &Port::number)
+            .def_readonly("name", &Port::name);
+
+        py::class_<CaptureManager>(m, "CaptureManager")
+            .def(py::init<std::vector<uint32_t>>())
+            .def("GetCam", &CaptureManager::GetCam)
+            .def("TakeAndSavePictures", &CaptureManager::TakeAndSavePictures);
     }
-*/
 }
--- usbcam/test.py
+++ usbcam/test.py
@@ -1,3 +1,5 @@
 import usbcam
 
+myList = usbcam.GetDevicesList()
 
+print(myList[0].name)
