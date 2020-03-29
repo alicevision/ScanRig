68788708d8d5ce82d6b954f1fa4d562d3dd1062a
guillaume-haerinck <guillaume.haerinck@outlook.com>
2020-03-29 17:20:10 +0200

    [usbcam] Prevent frame copy

--- usbcam/test.py
+++ usbcam/test.py
@@ -9,7 +9,7 @@ myCam0 = usbcam.CreateCamera(0)
 
 # Manipulate camera
 while(True):
-    buffer = np.frombuffer(myCam0.GetLastFrame(), dtype=np.ubyte)
+    buffer = np.array(myCam0.GetLastFrame(), dtype=np.ubyte, copy=False)
     frame = cv2.imdecode(buffer, cv2.IMREAD_COLOR)
     cv2.imshow('Example', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
